d23776823e85986df838d312ab0caf7ebe11e735
Nickolas Xisto Machado <cabocanick@gmail.com>
2025-07-06 16:40:35 -0300

    sla

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
 import os
+import contas
 import createDb as cdb
 import psycopg2
-<<<<<<< HEAD
-
-=======
 class conectar_banco:
-    
     def __init__(self):
         self.conection = cdb.connect_db()
     def connect_db():
     # Connect to your postgres DB
         conn = psycopg2.connect("host=localhost dbname=Linkado user=aplicacao password=senha123")
         return conn
->>>>>>> e08725faedfaa7cd9eed45579142783ad9eab484
 
 def clear_terminal():
     # Check if the operating system is Windows ('nt')
@@ -23,22 +19,26 @@ def clear_terminal():
 
 # Call the function to clear the terminal
 
+Query_map = {
+    'nome_query1': '''Insira a query aqui'''
+}
+
 def login():
-    clear_terminal()
     email =  input("digite o email \n")
     senha =  input("digite a senha \n")
-    query =  "select * from contas where email = %s and senha = %s"
-    conn =  cdb.connect_db()
-    res =  cdb.select_handler(conn, query, [email, senha])
-    print(res)
+    contas.Gerenciamento_Contas().login(email, senha)
 
 def register():
-    print("xabilson")
+    nome  =  input("digite o nome\n")
+    email =  input("digite o email \n")
+    senha =  input("digite a senha \n")
+    desc  = input("insira uma breve descricao sua \n")
+
+    
 
 
 def main():
    stop  = False
-   conn = cdb.connect_db()
    while not stop:
     inp =  int(input("escolha um tipo \n 1.Log-in \n 2.Criar Conta\n-1.Stop \n"))
     match  inp:
