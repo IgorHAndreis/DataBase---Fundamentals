76a51475f38f07a6c0a25d6b3d2c57eb580cddc0
IgorHAndreis <igorhenriqueandreis@gmail.com>
2025-07-06 18:55:39 -0300

    ADICAO FUNCAO DE INSERT DENTRO DO BANCO DE PUBLICAO

--- publicacoes.py
+++ publicacoes.py
@@ -3,14 +3,15 @@ from datetime import datetime
 
 class Publicacao:
 
-    def __init__(self, id: int, id_autor: int, texto_conteudo: str, anexo: str = None):
+    def __init__ (self, id: int, id_autor: int, texto_conteudo: str, anexo: str = None, id_grupo: int = None):
         self.id = id
         self.id_autor = id_autor
         self.texto_conteudo = texto_conteudo
         self.anexo = anexo
         self.data_hora = datetime.now()
+        self.id_grupo = id_grupo
 
-    def set_db_generated_attrs(self, novo_id: int):
+    def set_id(self, novo_id: int):
         self.id = novo_id
 
     def to_dict(self) -> dict:
@@ -19,7 +20,8 @@ class Publicacao:
             'idAutor': str(self.id_autor),
             'texto': self.texto_conteudo,
             'anexo': self.anexo,
-            'dataHora': self.data_hora
+            'dataHora': self.data_hora,
+            'id_grupo': self.id_grupo
         }
 
 class Gerenciamento_Publicacoes:
@@ -37,9 +39,9 @@ class Gerenciamento_Publicacoes:
             return None
 
         try:
-            self.insert_publicacao_banco(nova_publicacao)
+            nova_publicacao = self.insert_publicacao_banco(nova_publicacao)
             self.conexao.commit()
-            print(f"Publicação do autor ID {nova_publicacao.id_autor} criada com sucesso.")
+            print(f"Publicação de ID {nova_publicacao.id} criada com sucesso.")
             return nova_publicacao
         except (Exception, psycopg2.DatabaseError) as e:
             self.conexao.rollback()
@@ -48,7 +50,23 @@ class Gerenciamento_Publicacoes:
 
     def insert_publicacao_banco(self, publicacao: Publicacao):
         
-        pass
+        with self.conexao.cursor() as cursor:
+            query = '''
+                INSERT INTO publicacoes (id_autor, texto, anexos, data, id_grupo)
+                VALUES (%s, %s, %s, %s, %s)
+                RETURNING id;
+            '''
+            cursor.execute(query, (
+                publicacao.id_autor,
+                publicacao.texto_conteudo,  
+                publicacao.anexo,           
+                publicacao.data_hora,       
+                publicacao.id_grupo
+            ))
+            novo_id = cursor.fetchone()[0]
+            publicacao.set_id(novo_id)
+        
+        return publicacao
 
     def gerar_publicacao(self, data: dict) -> Publicacao:
     
@@ -57,7 +75,8 @@ class Gerenciamento_Publicacoes:
                 id=0,  
                 id_autor=data['id_autor'],
                 texto_conteudo=data['texto_conteudo'],
-                anexo=data.get('anexo')
+                anexo=data.get('anexo'),
+                id_grupo=data.get('id_grupo')
             )
             return nova_publicacao
         except KeyError as e:
