7ead85b51017f7bf0184138eec77496b0399c622
Nickolas Xisto Machado <cabocanick@gmail.com>
2025-07-06 22:09:15 -0300

    aaa

--- createDb.py
+++ createDb.py
@@ -3,7 +3,6 @@ import psycopg2
 
 
 def select_handler(conn:psycopg2.extensions.connection, query, args: list = []):
-    
     cur = conn.cursor()
     cur.execute(query, args)
     records = cur.fetchall()
